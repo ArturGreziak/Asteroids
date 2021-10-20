23a21996a76f7d08d39691eb30a70067b3f848e6
Artur Gręziak <alex4448@wp.pl>
2021-10-20 22:52:28 +0200

    Asteroids

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField]
+    string SceneName = "";
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            if (!string.IsNullOrEmpty(SceneName))
+                SceneManager.LoadScene(SceneName);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+}
